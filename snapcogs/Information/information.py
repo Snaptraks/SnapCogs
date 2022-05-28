198b3c2d605f78bc0b5368e9bb04bfb23aca0df9
Snaptraks <snaptraks@gmail.com>
2022-05-28 11:12:40 -0400

    Initial Information cog commit

---
+++ snapcogs/Information/information.py
@@ -0,0 +1,15 @@
+from discord.ext import commands
+
+
+class Information(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    # bot info (about)
+    # guild info
+    # user / member info
+    # user / member avatar
+    # charinfo
+    # timestamps?
+
+    # tips commands (or move to own module)
