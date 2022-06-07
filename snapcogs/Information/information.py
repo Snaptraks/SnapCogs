9c56c93b85e1a8f142c86fdebf80a3a54f51317d
Snaptraks <snaptraks@gmail.com>
2022-06-06 20:34:29 -0400

    Add about command

--- snapcogs/Information/information.py
+++ snapcogs/Information/information.py
@@ -1,3 +1,4 @@
+import asyncio
 import re
 from typing import Union
 import unicodedata
@@ -7,7 +8,7 @@ from discord import app_commands
 from discord.ext import commands
 import pint
 
-from ..utils import relative_dt
+from ..utils import relative_dt, run_process
 
 
 UREG = pint.UnitRegistry()
@@ -54,6 +55,10 @@ async def to_units_autocomplete(
     return choice_units
 
 
+def int_fmt(number, digits=3):
+    return f"`{number:>{digits}d}`"
+
+
 class Badge:
     """From https://github.com/python-discord/bot/blob/80a5b83f4fb0fd1204e051954a3c1b1b914fe7bc/config-default.yml#L43-L53"""  # noqa: E501
 
@@ -71,7 +76,6 @@ class Badge:
 
 class Information(commands.Cog):
     # bot info (about)
-    # guild info
     # timestamps?
 
     # tips commands (or move to own module)
@@ -88,9 +92,83 @@ class Information(commands.Cog):
         )
         self.bot.tree.add_command(self.info_user_context_menu)
 
+    @app_commands.command()
+    async def about(self, interaction: discord.Interaction):
+        """View information about the bot itself."""
+
+        app_info = await self.bot.application_info()
+
+        # host should have git installed if they installed this package
+        git_link, _ = await run_process("git config --get remote.origin.url")
+
+        embed = discord.Embed(
+            title=f"Information about {self.bot.user}.",
+            description=app_info.description,
+            url=git_link,
+            color=discord.Color.blurple(),
+        ).set_thumbnail(url=self.bot.user.avatar.url)
+
+        # some statistics
+        total_members = 0
+        total_online = 0
+        offline = discord.Status.offline
+        for member in self.bot.get_all_members():
+            total_members += 1
+            if member.status is not offline:
+                total_online += 1
+
+        total_unique = len(self.bot.users)
+
+        text_channels = 0
+        voice_channels = 0
+        guilds = 0
+        for guild in self.bot.guilds:
+            guilds += 1
+            for channel in guild.channels:
+                if isinstance(channel, discord.TextChannel):
+                    text_channels += 1
+                elif isinstance(channel, discord.VoiceChannel):
+                    voice_channels += 1
+
+        embed.add_field(
+            name="Members",
+            value=(
+                f"{int_fmt(total_members)} Total\n"
+                f"{int_fmt(total_unique)} Unique\n"
+                f"{int_fmt(total_online)} Online"
+            ),
+        )
+        embed.add_field(
+            name="Channels",
+            value=(
+                f"{int_fmt(text_channels + voice_channels)} Total\n"
+                f"{int_fmt(text_channels)} Text\n"
+                f"{int_fmt(voice_channels)} Voice"
+            ),
+        )
+        embed.add_field(
+            name="Servers", value=int_fmt(guilds),
+        )
+        embed.add_field(
+            name="Timeline",
+            value=(
+                f"Boot time: {relative_dt(self.bot.boot_time)}\n"
+                f"Created: {relative_dt(self.bot.user.created_at)}\n"
+                f"Joined server: {relative_dt(interaction.guild.me.joined_at)}"
+            ),
+        )
+
+        embed.set_footer(
+            text=f"Made with discord.py v{discord.__version__} by {app_info.owner}",
+            icon_url="http://i.imgur.com/5BFecvA.png",
+        )
+        embed.timestamp = discord.utils.utcnow()
+
+        await interaction.response.send_message(embed=embed)
+
     @info.command(name="server")
     async def info_guild(self, interaction: discord.Interaction):
-        """Get information about the current server."""
+        """View information about the current server."""
 
         guild = interaction.guild
         embed = discord.Embed(title=guild.name, color=discord.Color.blurple())
@@ -107,20 +185,22 @@ class Information(commands.Cog):
 
         embed.add_field(
             name="Members Info",
-            value=(f"Members: {guild.member_count}\n" f"Roles: {len(guild.roles)-1}\n"),
-        ).add_field(
-            name="Boosts",
             value=(
-                f"Nitro Boosters: {len(guild.premium_subscribers)}\n"
-                f"Nitro Boosts: {guild.premium_subscription_count}\n"
-                f"Nitro level: {guild.premium_tier}\n"
+                f"{int_fmt(guild.member_count)} Members\n"
+                f"{int_fmt(len(guild.roles)-1)} Roles\n"
+            ),
+        ).add_field(
+            name=f"Nitro Level {guild.premium_tier}",
+            value=(
+                f"{int_fmt(len(guild.premium_subscribers))} Nitro Boosters\n"
+                f"{int_fmt(guild.premium_subscription_count)} Nitro Boosts\n"
             ),
         ).add_field(
             name="Channels",
             value=(
-                f"`{len(guild.text_channels):>3d}` Text Channels\n"
-                f"`{len(guild.voice_channels):>3d}` Voice Channels\n"
-                f"`{len(guild.stage_channels):>3d}` Stage Channels\n"
+                f"{int_fmt(len(guild.text_channels))} Text\n"
+                f"{int_fmt(len(guild.voice_channels))} Voice\n"
+                f"{int_fmt(len(guild.stage_channels))} Stage\n"
             ),
         )
 
--- snapcogs/utils/__init__.py
+++ snapcogs/utils/__init__.py
@@ -1,5 +1,19 @@
+import asyncio
 import discord
 
 
 def relative_dt(dt):
+    """Format the datetime in the relative timestamp form."""
+
     return discord.utils.format_dt(dt, style="R")
+
+
+async def run_process(command):
+    """Run a command in shell. To be used carefully!"""
+
+    process = await asyncio.create_subprocess_shell(
+        command, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE,
+    )
+    result = await process.communicate()
+
+    return [output.decode("utf-8", "ignore") for output in result]
