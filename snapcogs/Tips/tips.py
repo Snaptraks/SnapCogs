1e7438eb33d3a32fe13e9ba11ffb3442b3be96c1
Snaptraks <snaptraks@gmail.com>
2022-06-14 10:57:23 -0400

    Add tip show command

--- snapcogs/Tips/tips.py
+++ snapcogs/Tips/tips.py
@@ -14,6 +14,19 @@ class Tips(commands.Cog):
         name="tip", description="Save and share tips for people on the server!"
     )
 
+    async def tip_name_autocomplete(
+        self, interaction: discord.Interaction, current: str
+    ):
+        rows = await self._get_tips_names_like(current, interaction.guild)
+        suggestions = [
+            app_commands.Choice(name=row["name"], value=row["name"])
+            for row in rows[:25]
+        ]
+        LOGGER.debug(
+            f"tip_name_autocomplete: {current=}, {len(suggestions)} suggestions"
+        )
+        return suggestions
+
     def __init__(self, bot) -> None:
         self.bot = bot
 
@@ -21,7 +34,9 @@ class Tips(commands.Cog):
         await self._create_tables()
 
     @tip.command(name="create")
-    async def create(self, interaction: discord.Interaction):
+    async def tip_create(self, interaction: discord.Interaction):
+        """Create a new tip for the current server, owned by you."""
+
         LOGGER.debug(f"Creating a tip in guild {interaction.guild}.")
         modal = views.TipCreate()
         await interaction.response.send_modal(modal)
@@ -29,6 +44,7 @@ class Tips(commands.Cog):
         LOGGER.debug(f"Tip {modal.name.value!r} received.")
 
         payload = dict(
+            author_id=interaction.user.id,
             content=modal.content.value,
             created_at=interaction.created_at,
             guild_id=interaction.guild.id,
@@ -37,22 +53,52 @@ class Tips(commands.Cog):
 
         await self._save_tip(payload)
 
+    @tip.command(name="show")
+    @app_commands.describe(name="Name of the tip.")
+    @app_commands.autocomplete(name=tip_name_autocomplete)
+    async def tip_show(self, interaction: discord.Interaction, name: str):
+        """Show a tip in the current channel."""
+
+        tip = await self._get_tip_by_name(name, interaction.guild)
+
+        if tip is None:
+            await interaction.response.send_message(
+                f"No tip named `{name}` here!", ephemeral=True
+            )
+            return
+
+        tip_author = interaction.guild.get_member(tip["author_id"])
+        embed = (
+            discord.Embed(
+                title=f"Tip {tip['name']}",
+                description=tip["content"],
+                color=discord.Color.blurple(),
+                timestamp=tip["created_at"],
+            )
+            .set_author(name=tip_author, icon_url=tip_author.display_avatar.url)
+            .set_footer(text=f"Tip used {tip['uses'] + 1} times")
+        )
+
+        await interaction.response.send_message(embed=embed)
+
+        await self._increase_tip_uses(tip["tip_id"])
+
     async def _create_tables(self):
         """Create the necessary database tables."""
 
         await self.bot.db.execute(
             """
-                CREATE TABLE IF NOT EXISTS tips_tip(
-                    content    TEXT     NOT NULL,
-                    created_at DATETIME NOT NULL,
-                    guild_id   INTEGER  NOT NULL,
-                    name       TEXT     NOT NULL,
-                    tip_id     INTEGER  NOT NULL PRIMARY KEY,
-                    uses       INTEGER  DEFAULT 0 NOT NULL
-                )
-                """,
+            CREATE TABLE IF NOT EXISTS tips_tip(
+                author_id  INTEGER  NOT NULL,
+                content    TEXT     NOT NULL,
+                created_at DATETIME NOT NULL,
+                guild_id   INTEGER  NOT NULL,
+                name       TEXT     NOT NULL,
+                tip_id     INTEGER  NOT NULL PRIMARY KEY,
+                uses       INTEGER  DEFAULT 0 NOT NULL
+            )
+            """,
         )
-
         await self.bot.db.commit()
 
     async def _save_tip(self, payload):
@@ -60,16 +106,64 @@ class Tips(commands.Cog):
 
         await self.bot.db.execute(
             """
-            INSERT INTO tips_tip(content, 
+            INSERT INTO tips_tip(author_id,
+                                 content, 
                                  created_at, 
                                  guild_id, 
                                  name)
-            VALUES (:content,
+            VALUES (:author_id,
+                    :content,
                     :created_at,
                     :guild_id,
                     :name)
             """,
             payload,
         )
-        LOGGER.debug(f"Tip {payload['name']} saved.")
         await self.bot.db.commit()
+        LOGGER.debug(f"Tip {payload['name']} saved.")
+
+    async def _get_tip_by_name(self, name: str, guild: discord.Guild):
+        """Get a tip by its name in the current guild."""
+
+        async with self.bot.db.execute(
+            """
+            SELECT * 
+              FROM tips_tip
+             WHERE guild_id=:guild_id
+               AND name=:name
+            """,
+            dict(guild_id=guild.id, name=name),
+        ) as c:
+            row = await c.fetchone()
+
+        if row:
+            LOGGER.debug(f"Found tip {row['name']} for guild {row['guild_id']}.")
+
+        return row
+
+    async def _get_tips_names_like(self, substring: str, guild: discord.Guild):
+        """Get a list of tip names that contain substring."""
+
+        return await self.bot.db.execute_fetchall(
+            """
+            SELECT name
+              FROM tips_tip
+             WHERE INSTR(name, :substring) > 0
+               AND guild_id=:guild_id
+            """,
+            dict(substring=substring, guild_id=guild.id),
+        )
+
+    async def _increase_tip_uses(self, tip_id: int):
+        """Increase the use of tip with tip_id by 1."""
+
+        await self.bot.db.execute(
+            """
+            UPDATE tips_tip
+               SET uses = uses + 1
+             WHERE tip_id=:tip_id
+            """,
+            dict(tip_id=tip_id),
+        )
+        await self.bot.db.commit()
+        LOGGER.debug(f"Increased uses for {tip_id=}")
