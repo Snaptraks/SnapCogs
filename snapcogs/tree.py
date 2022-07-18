4c2e88a0cde7b2f5fe7917e91250c0a58b37eeaf
Snaptraks <snaptraks@gmail.com>
2022-07-18 11:43:37 -0400

    Add docstring to CommandTree.on_error

--- snapcogs/tree.py
+++ snapcogs/tree.py
@@ -10,7 +10,24 @@ class CommandTree(app_commands.CommandTree):
     async def on_error(
         self, interaction: discord.Interaction, error: app_commands.AppCommandError
     ):
+        """Error handler that sends unhandled errors to the logger.
+
+        To make sure errors are logged here even when application commands have
+        an error handler, you should use the following pattern for the handler:
+
+        ```py
+        async def error_handler(interaction: discord.Interaction, error: Exception):
+            if isinstance(error, ...):
+                # do something here
+            elif isinstance(error, ...):
+                # for another type of exception
+            else:
+                # this is the important part
+                interaction.extras["error_handled"] = False
+        ```
+        """
         command = interaction.command
+        # assume the error is handled by default, unless explicitely set to False
         error_handled = interaction.extras.get("error_handled", True)
 
         if command is not None:
