e919e1c8e3733368e3397701c9643566f07a5da5
Snaptraks <snaptraks@gmail.com>
2022-08-22 10:18:24 -0400

    Update logger to discord.py v2.0 location

--- snapcogs/__init__.py
+++ snapcogs/__init__.py
@@ -7,8 +7,8 @@ from snapcogs.bot import Bot  # noqa: F401
 
 LOGGER = logging.getLogger("snapcogs")
 LOG_HANDLER = logging.StreamHandler()
-if discord.client.stream_supports_colour(LOG_HANDLER.stream):
-    LOG_FORMAT = discord.client._ColourFormatter()
+if discord.utils.stream_supports_colour(LOG_HANDLER.stream):
+    LOG_FORMAT = discord.utils._ColourFormatter()
 else:
     LOG_FORMAT = logging.Formatter(
         "%(asctime)s : %(levelname)s : %(name)s : %(message)s"
