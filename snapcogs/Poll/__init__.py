38cbb7740456d69aa6171eaf383c10f215307396
Snaptraks <snaptraks@gmail.com>
2024-09-13 13:26:07 -0400

    Remove Poll cog
    Polls are now available natively in Discord

--- snapcogs/Poll/__init__.py
+++
@@ -1,6 +0,0 @@
-from ..bot import Bot
-from .poll import Poll
-
-
-async def setup(bot: Bot):
-    await bot.add_cog(Poll(bot))
--- snapcogs/__init__.py
+++ snapcogs/__init__.py
@@ -1,6 +1,6 @@
 from snapcogs.bot import Bot  # noqa: F401
 from snapcogs.utils.logging import get_logger
 
-__version__ = "24.09.1"
+__version__ = "24.09.2"
 
 LOGGER = get_logger("snapcogs")
