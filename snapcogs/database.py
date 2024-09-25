8432bd2efab4493cdd362eb6969e34d7930b60bb
Snaptraks <snaptraks@gmail.com>
2024-09-25 13:50:48 +0000

    Use URL.create to define database url

--- snapcogs/database.py
+++ snapcogs/database.py
@@ -1,3 +1,4 @@
+from sqlalchemy import URL
 from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
@@ -7,11 +8,10 @@ class Base(DeclarativeBase):
 
 
 class Database:
-    def __init__(self, database_url: str | None = None) -> None:
-        if database_url is None:
-            database_url = "sqlite+aiosqlite://"
+    def __init__(self, database_name: str | None = None) -> None:
+        database_url = URL.create("sqlite+aiosqlite", database=database_name)
 
-        self.engine = create_async_engine(database_url, echo=True)
+        self.engine = create_async_engine(database_url)
         self.session = async_sessionmaker(self.engine, expire_on_commit=False)
 
     async def initialise_database(self) -> None:
